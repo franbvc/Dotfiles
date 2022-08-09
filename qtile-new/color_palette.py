7713fe788726c1c6d072f017b397d1596ab73ae2
franbvc <franbvc@gmail.com>
2022-08-08 23:25:37 -0300

    feat(file): qtile color palette

---
+++ qtile-new/color_palette.py
@@ -0,0 +1,33 @@
+color_dict = { 
+    "terminal_red": "#f7768e",
+    "orange": "#ff9e64",
+    "terminal_yellow": "#e0af68",
+    "green": "#9ece6a",
+    "terminal_green": "#73daca",
+    "blue_1": "#b4f9f8",
+    "blue_2": "#2ac3de",
+    "terminal_cyan": "#7dcfff",
+    "terminal_blue": "#7aa2f7",
+    "terminal_magenta": "#bb9af7",
+    "terminal_white": "#c0caf5",
+    "foreground": "#a9b1d6",
+    "foreground_2": "#9aa5ce",
+    "cream": "#cfc9c2",
+    "comments": "#565f89",
+    "terminal_black": "#414868",
+    "background_storm": "#24283b",
+    "background_night": "#1a1b26",
+}
+
+simple_dict = {
+    "red": color_dict["terminal_red"],
+    "orange": color_dict["orange"],
+    "yellow": color_dict["terminal_yellow"],
+    "green": color_dict["terminal_green"],
+    "cyan": color_dict["terminal_cyan"],
+    "blue": color_dict["terminal_blue"],
+    "magenta": color_dict["terminal_magenta"],
+    "white": color_dict["terminal_white"],
+    "black": color_dict["terminal_black"],
+}
+
